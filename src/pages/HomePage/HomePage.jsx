4f4f3b94703f2e07fa2974e5b8a213d705abd591
Carl <qwerfcxzas1@gmail.com>
2022-11-14 23:50:35 +0800

    modif

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './homePage.scss'
+function HomePage() {
+  return (
+    <div className="homePage">
+      <div className="leftDownBg"></div>
+      <div className="leftDown1"></div>
+      <div className="rightUp"></div>
+      <nav>
+        <div className="logo"></div>
+        <button>歷史紀錄</button>
+      </nav>
+      <div className="container">
+        <div className="rightDown"></div>
+        <div className="right1"></div>
+        <div className="right2"></div>
+        <div className="right3"></div>
+        <div className="right5"></div>
+        <div className="right4"></div>
+      </div>
+    </div>
+  )
+}
+
+export default HomePage

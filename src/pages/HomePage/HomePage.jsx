a585b57afcd2fb1d5d2f8da8964e101cbf2299c4
Carl <carlhuang@sunnygo.com.tw>
2022-11-15 09:18:18 +0800

    modify

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import './homePage.scss'
+import rightDown from '../images/rightDown.png'
+import right1 from '../images/right1.png'
+import right2 from '../images/right2.png'
+import right3 from '../images/right3.png'
+import right4 from '../images/right4.png'
+import right5 from '../images/right5.png'
 function HomePage() {
   return (
     <div className="homePage">
@@ -10,13 +16,18 @@ function HomePage() {
         <div className="logo"></div>
         <button>歷史紀錄</button>
       </nav>
+      <div></div>
       <div className="container">
-        <div className="rightDown"></div>
-        <div className="right1"></div>
-        <div className="right2"></div>
-        <div className="right3"></div>
-        <div className="right5"></div>
-        <div className="right4"></div>
+        <div className="imgContainer">
+          <div>
+            <img src={rightDown} alt="" className="rightDown" />
+            <img src={right1} alt="" className="right1" />
+            <img src={right2} alt="" className="right2" />
+            <img src={right3} alt="" className="right3" />
+            <img src={right5} alt="" className="right5" />
+            <img src={right4} alt="" className="right4" />
+          </div>
+        </div>
       </div>
     </div>
   )

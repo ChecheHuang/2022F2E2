753181102e22c4dd6c4c80952c7375a9af73f3a9
Carl <carlhuang@sunnygo.com.tw>
2022-11-16 09:05:36 +0800

    modify

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -8,11 +8,42 @@ import right4 from '../images/right4.png'
 import right5 from '../images/right5.png'
 import dragFileIcon from '../images/dragFileIcon.png'
 import { useState } from 'react'
+import { useDropzone } from 'react-dropzone'
 function HomePage() {
   const [openModel, setOpenModel] = useState(false)
+  const [files, setFiles] = useState([])
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: 'image/*',
+    onDrop: (acceptFiles) => {
+      setFiles(
+        acceptFiles.map((file) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        )
+      )
+    },
+  })
 
+  const handleUploadFile = (e) => {
+    if (!e.target.files[0]) return
+    var reader = new FileReader()
+    reader.onload = function () {
+      setFiles([reader.result])
+    }
+    reader?.readAsDataURL(e?.target?.files[0])
+    e.target.value = ''
+  }
+  const images = files.map((file) => {
+    return (
+      <div key={file.name}>
+        <img src={file.preview} style={{ width: '200px' }} alt="" />
+      </div>
+    )
+  })
   return (
     <div className="homePage">
+      <div>{images}</div>
       <div className="leftDownBg"></div>
       <div className="leftDown1"></div>
       <div className="rightUp"></div>
@@ -43,10 +74,16 @@ function HomePage() {
           <br />
           省時便利又環保。
         </div>
-        <div className="fileContainer">
+        <div {...getRootProps()} className="fileContainer">
           <img src={dragFileIcon} alt="" />
-          <label htmlFor="upload">選擇檔案</label>
-          <input type="file" id="upload" style={{ display: 'none' }} />
+          <label htmlFor="">選擇檔案</label>
+          <input
+            {...getInputProps()}
+            // onChange={handleUploadFile}
+            type="file"
+            id="upload"
+            style={{ display: 'none' }}
+          />
           <div className="remind">或拖移檔案到此處</div>
           <div className="remind2">(限10MB內的PDF或JPG檔)</div>
         </div>

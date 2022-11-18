7872c0d96caa2977dc09399cc5d60b5f2a489f05
Carl <carlhuang@sunnygo.com.tw>
2022-11-18 09:04:56 +0800

    moidfy

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -9,7 +9,10 @@ import right5 from '../images/right5.png'
 import dragFileIcon from '../images/dragFileIcon.png'
 import { useState } from 'react'
 import { useDropzone } from 'react-dropzone'
+import { useLottie } from 'lottie-react'
+import loadingLottie from '../../GNsign_loading.json'
 function HomePage() {
+  const [loading, setLoading] = useState(false)
   const [openModel, setOpenModel] = useState(false)
   const [modelInfo, setModelInfo] = useState('')
   const [files, setFiles] = useState([])
@@ -40,6 +43,11 @@ function HomePage() {
   const images = files.map((file) => {
     return <img key={file.name} src={file.preview} alt="" />
   })
+  const options = {
+    animationData: loadingLottie,
+    loop: true,
+  }
+  const { View } = useLottie(options)
   return (
     <div className="homePage">
       <div className="leftDownBg"></div>
@@ -63,41 +71,51 @@ function HomePage() {
       )}
 
       <div className="container">
-        <button>免費試用版</button>
-        <div className="title">小綠簽</div>
-        <div className="description">
-          護樹、永續、減碳的綠色生活
-          <br />
-          響應環保無紙化電子簽屬，
-          <br />
-          省時便利又環保。
-        </div>
-        <div {...getRootProps()} className="fileContainer">
-          <div className="filImgContainer">
-            {images.length === 0 ? <img src={dragFileIcon} alt="" /> : images}
-          </div>
+        {loading ? (
+          <div className="loadingContainer">{View}</div>
+        ) : (
+          <>
+            <button>免費試用版</button>
+            <div className="title">小綠簽</div>
+            <div className="description">
+              護樹、永續、減碳的綠色生活
+              <br />
+              響應環保無紙化電子簽屬，
+              <br />
+              省時便利又環保。
+            </div>
+            <div {...getRootProps()} className="fileContainer">
+              <div className="filImgContainer">
+                {images.length === 0 ? (
+                  <img src={dragFileIcon} alt="" />
+                ) : (
+                  images
+                )}
+              </div>
 
-          <label htmlFor="">選擇檔案</label>
-          <input
-            {...getInputProps()}
-            // onChange={handleUploadFile}
-            type="file"
-            id="upload"
-            style={{ display: 'none' }}
-          />
-          <div className="remind">或拖移檔案到此處</div>
-          <div className="remind2">(限10MB內的PNG或JPG檔)</div>
-        </div>
-        <div className="imgContainer">
-          <div>
-            <img src={rightDown} alt="" className="rightDown" />
-            <img src={right1} alt="" className="right1" />
-            <img src={right2} alt="" className="right2" />
-            <img src={right3} alt="" className="right3" />
-            <img src={right5} alt="" className="right5" />
-            <img src={right4} alt="" className="right4" />
-          </div>
-        </div>
+              <label htmlFor="">選擇檔案</label>
+              <input
+                {...getInputProps()}
+                // onChange={handleUploadFile}
+                type="file"
+                id="upload"
+                style={{ display: 'none' }}
+              />
+              <div className="remind">或拖移檔案到此處</div>
+              <div className="remind2">(限10MB內的PNG或JPG檔)</div>
+            </div>
+            <div className="imgContainer">
+              <div>
+                <img src={rightDown} alt="" className="rightDown" />
+                <img src={right1} alt="" className="right1" />
+                <img src={right2} alt="" className="right2" />
+                <img src={right3} alt="" className="right3" />
+                <img src={right5} alt="" className="right5" />
+                <img src={right4} alt="" className="right4" />
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </div>
   )

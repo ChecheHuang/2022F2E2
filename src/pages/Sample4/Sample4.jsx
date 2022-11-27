7372257bc845bebeec4a74e9f142dabae43505b9
Carl <carlhuang@sunnygo.com.tw>
2022-11-27 21:58:51 +0800

    modify

--- src/pages/Sample4/Sample4.jsx
+++ src/pages/Sample4/Sample4.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import PdfViewerComponent from './PdfViewerComponent'
 function Sample4() {
+  const [pdfFile, setPdfFile] = useState(null)
+  const handleFile = (e) => {
+    let selectedFile = e.target.files[0]
+    // console.log(selectedFile.type)
+    // console.log(selectedFile)
+    const reader = new FileReader()
+    reader.readAsDataURL(selectedFile)
+    reader.onloadend = (e) => {
+      setPdfFile(e.target.result)
+    }
+  }
   return (
-    <div>Sample4</div>
+    <div>
+      <div className="PDF-viewer">
+        <PdfViewerComponent document={'document.pdf'} />
+      </div>
+    </div>
   )
 }
 
